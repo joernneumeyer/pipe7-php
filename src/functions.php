1b7d2705e88fb8d11675c524ad4ccefd5ecc7a73
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-05-16 18:46:15 +0200

    add take function to fetch items from an iterator as they are needed
    Signed-off-by: Jörn Neumeyer <contact@joern-neumeyer.de>

--- src/functions.php
+++ src/functions.php
@@ -14,3 +14,21 @@
   function pipe($data): CollectionPipe {
     return CollectionPipe::from($data);
   }
+
+  function take(Iterator $iterator, ?int $n = null) {
+    if ($n === null) {
+      return function (int $n) use ($iterator) {
+        $result = [];
+        foreach ($iterator as $i) {
+          if (--$n > -1) {
+            $result[] = $i;
+          } else {
+            break;
+          }
+        }
+        return $result;
+      };
+    } else {
+      return take($iterator)($n);
+    }
+  }
