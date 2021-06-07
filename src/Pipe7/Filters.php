45f0382285c0ad87b6fe8ecb013bdae9ad2f9fa8
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-06-07 18:59:21 +0200

    add more filters with tests
    Signed-off-by: Jörn Neumeyer <contact@joern-neumeyer.de>

--- src/Pipe7/Filters.php
+++ src/Pipe7/Filters.php
@@ -2,6 +2,7 @@
 
   namespace Neu\Pipe7;
 
+  use Closure;
   use Neu\Pipe7\StatefulOperators\Limit;
 
   /**
@@ -11,10 +12,71 @@
   final class Filters {
 
     /**
-     * @param $options
+     * Limits the amount of elements emitted after the filter to the number of elements specified.
+     * @param int $count
      * @return Limit
      */
-    public static function limit($count) {
+    public static function limit(int $count): Limit {
       return (new Limit())->maxLength($count);
+    }
+
+    /**
+     * Checks if an array or string contains the specified needle.
+     * @param mixed $needle
+     * @return Closure
+     */
+    public static function contains($needle): Closure {
+      return function($value) use ($needle) {
+        if (is_array($value)) {
+          return in_array($needle, $value);
+        } else if (is_string($needle)) {
+          return strpos($value, $needle) !== false;
+        } else {
+          throw new \InvalidArgumentException('Cannot check if type "' . gettype($value) . '" contains a needle!');
+        }
+      };
+    }
+
+    /**
+     * Checks if an array or string starts with the specified needle.
+     *
+     * If the value is an array, it is expected to have numerical indices, starting with 0.
+     * @param mixed $needle
+     * @return Closure
+     */
+    public static function startsWith($needle): Closure {
+      return function($value) use ($needle) {
+        if (is_array($value)) {
+          return $value[0] === $needle;
+        } else if (is_string($needle)) {
+          return strpos($value, $needle) === 0;
+        }
+        else {
+          throw new \InvalidArgumentException('Cannot check if type "' . gettype($value) . '" contains a needle!');
+        }
+      };
+    }
+
+    /**
+     * Checks if an array or string ends with the specified needle.
+     *
+     * If the value is an array, it is expected to have numerical indices, starting with 0.
+     * @param mixed $needle
+     * @return Closure
+     */
+    public static function endsWith($needle) {
+      return function($value) use ($needle) {
+        if (is_array($value)) {
+          return $value[count($value) - 1] === $needle;
+        } else if (is_string($needle)) {
+          $valueLength = strlen($value);
+          $needleLength = strlen($needle);
+          $needleIndex = strpos($value, $needle);
+          return $valueLength - $needleLength === $needleIndex;
+        }
+        else {
+          throw new \InvalidArgumentException('Cannot check if type "' . gettype($value) . '" contains a needle!');
+        }
+      };
     }
   }
--- tests/FiltersTest.php
+++ tests/FiltersTest.php
@@ -1,8 +1,67 @@
 <?php
 
+  use Neu\Pipe7\Filters;
+  use Neu\Pipe7\Sources;
   use function Neu\Pipe7\pipe;
 
-  it('should limit the result to 5 elements', function() {
-    $result = pipe(\Neu\Pipe7\Sources::range(50))->filter(\Neu\Pipe7\Filters::limit(5))->toArray();
+  it('should limit the result to 5 elements', function () {
+    $result = pipe(Sources::range(50))->filter(Filters::limit(5))->toArray();
     expect(count($result))->toEqual(5);
   });
+
+  it('contains - should only return strings containing the specified string', function () {
+    $data     = ['Hello', 'World', 'foo', 'bar', 'foobar', 'salut', 'mark'];
+    $expected = [3 => 'bar', 4 => 'foobar', 6 => 'mark'];
+    $result   = pipe($data)->filter(Filters::contains('ar'))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('contains - should only return arrays containing the specified item', function () {
+    $data     = [[2, 3, 4], [7, 2, 6], [5, 5], [6], [3, 2, 6, 7]];
+    $expected = [1 => [7, 2, 6], 3 => [6], 4 => [3, 2, 6, 7]];
+    $result   = pipe($data)->filter(Filters::contains(6))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('contains - should throw if an unusable type shall be searched', function () {
+    $data = [100, 324, 678];
+    pipe($data)->filter(Filters::contains(1))->toArray();
+  })->throws(InvalidArgumentException::class);
+
+  it('startsWith - should only return string starting with the specified string', function () {
+    $data     = ['funny', 'World', 'foo', 'bar', 'foobar', 'salut', 'mark'];
+    $expected = [0 => 'funny', 2 => 'foo', 4 => 'foobar'];
+    $result   = pipe($data)->filter(Filters::startsWith('f'))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('startsWith - should only return arrays starting with the specified element', function () {
+    $data     = [[4, 6], [7], [2, 6, 3], [4, 1, 2, 7], [5]];
+    $expected = [0 => [4, 6], 3 => [4, 1, 2, 7]];
+    $result   = pipe($data)->filter(Filters::startsWith(4))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('startsWith - should throw if an unusable type shall be searched', function () {
+    $data = [100, 324, 678];
+    pipe($data)->filter(Filters::startsWith(1))->toArray();
+  })->throws(InvalidArgumentException::class);
+
+  it('endsWith - should only return string starting with the specified string', function () {
+    $data     = ['funny', 'World', 'foo', 'bar', 'foobar', 'salut', 'mark'];
+    $expected = [3 => 'bar', 4 => 'foobar'];
+    $result   = pipe($data)->filter(Filters::endsWith('bar'))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('endsWith - should only return arrays starting with the specified element', function () {
+    $data     = [[4, 6], [7], [2, 6, 3], [4, 1, 2, 7], [5]];
+    $expected = [1=>[7], 3 => [4, 1, 2, 7]];
+    $result   = pipe($data)->filter(Filters::endsWith(7))->toArray();
+    expect($result)->toMatchArray($expected);
+  });
+
+  it('endsWith - should throw if an unusable type shall be searched', function () {
+    $data = [100, 324, 678];
+    pipe($data)->filter(Filters::endsWith(1))->toArray();
+  })->throws(InvalidArgumentException::class);
