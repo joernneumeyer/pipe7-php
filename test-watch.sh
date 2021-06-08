a57ef0b0ab7e29497e7277768dc953caf4d7ff72
joernneumeyer <contact@joern-neumeyer.de>
2021-06-08 08:17:10 +0000

    Deploying to gh-pages from @ joernneumeyer/pipe7-php@06eb22bd44124c26c6fc6e85e4bc412090b3c2bb 🚀

--- test-watch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-composer test
-while inotifywait -e modify,create tests/ -r
-do
-  composer test
-done
--- tests/SourcesTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  use function Neu\Pipe7\pipe;
-
-  it('should generate proper primes', function() {
-    $correct = [2,3,5,7,11,13,17,19,23,29];
-    $calculated = pipe(\Neu\Pipe7\Sources::primes())->filter(\Neu\Pipe7\Filters::limit(10))->toArray();
-    expect($calculated)->toMatchArray($correct);
-  });
