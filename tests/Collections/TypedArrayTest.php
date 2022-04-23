829004759e94eab84f2a6b9b7e7a487030b7a8b4
Jörn Neumeyer <contact@joern-neumeyer.de>
2022-04-23 09:13:42 +0200

    add tests for coverage
    Signed-off-by: Jörn Neumeyer <contact@joern-neumeyer.de>

--- tests/Collections/TypedArrayTest.php
+++ tests/Collections/TypedArrayTest.php
@@ -32,7 +32,7 @@
     [SimpleEntity::class, new OtherEntity(20)],
   ];
 
-  it('should accept valid values', function($type, $expected) {
+  it('should accept valid values', function ($type, $expected) {
     $arr = TypedArray::forType($type);
     foreach ($expected as $e) {
       $arr[] = $e;
@@ -40,11 +40,33 @@
     expect($arr->unwrapped())->toMatchArray($expected);
   })->with($validCases);
 
-  it('should not accept invalid values', function($type, $value) {
-    $arr = TypedArray::forType($type);
+  it('should not accept invalid values', function ($type, $value) {
+    $arr   = TypedArray::forType($type);
     $arr[] = $value;
   })->with($invalidCases)->throws(InvalidArgumentException::class);
 
-  it('should throw if an undefined type is passed', function() {
+  it('should throw if an undefined type is passed', function () {
     TypedArray::forType('foobar123');
   })->throws(InvalidArgumentException::class);
+
+  it('should return the proper type', function () {
+    $arr = TypedArray::forType('int');
+    expect($arr->type())->toEqual('int');
+  });
+
+  it('should return a proper iterator', function () {
+    $arr = TypedArray::forType('int');
+    $arr[] = 42;
+    $arr[] = 567;
+    $arr[] = 21;
+    $result = iterator_to_array($arr);
+    expect($result)->toMatchArray([42, 567, 21]);
+  });
+
+  it('should push elements to their proper index', function() {
+    $arr = TypedArray::forType('int');
+    $arr['hello'] = 55;
+    $arr['world'] = 65;
+    $result = iterator_to_array($arr);
+    expect($result)->toMatchArray(['hello' => 55, 'world' => 65]);
+  });
